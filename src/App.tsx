04a841c327e784d2a5198b9d97f95d98d93c8a04
Victor Kozykin <kozykin_victor_95@mail.ru>
2022-05-03 22:59:17 +0700

    feat: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+
+import React from 'react';
+import { Button } from '@mui/material';
 
 function App() {
-      const a = 'sdf';
+  const a = 'sdf';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-6xl text-gray-600">asdasd</div>
+      <Button>sdfsdf</Button>
     </div>
   );
 }
